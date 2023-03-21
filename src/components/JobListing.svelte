832c404aef259b265cdbe71114834ee86afa3aa0
Eric Grossé <ericgrosse1@gmail.com>
2023-03-21 17:03:06 -0400

    added styling

--- src/components/JobListing.svelte
+++ src/components/JobListing.svelte
@@ -15,3 +15,36 @@
     <li><strong>Salary:</strong> {jobSalary}</li>
   </ul>
 </div>
+
+<style>
+  .job-listing {
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    max-width: 400px;
+    width: 100%;
+    text-align: left;
+  }
+
+  h2 {
+    text-align: center;
+    margin-top: 0;
+    margin-bottom: 10px;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    margin-bottom: 5px;
+  }
+
+  li strong {
+    width: 80px;
+    display: inline-block;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,44 @@
   import { jobListings } from '../data/jobListings.js';
 </script>
 
-<h1>Job Listings</h1>
+<div class="container">
+  <h1 style="margin-bottom:0;"><strong><i>CareerHub</i></strong></h1>
+  <h1 style="margin-top:40px;">Job Listings</h1>
 
-{#each jobListings as jobListing}
-  <JobListing
-    jobTitle={jobListing.title}
-    jobDescription={jobListing.description}
-    jobLocation={jobListing.location}
-    jobType={jobListing.type}
-    jobSalary={jobListing.salary}
-  />
-{/each}
+  <div class="job-listings">
+    {#each jobListings as jobListing}
+      <JobListing
+        jobTitle={jobListing.title}
+        jobDescription={jobListing.description}
+        jobLocation={jobListing.location}
+        jobType={jobListing.type}
+        jobSalary={jobListing.salary}
+      />
+    {/each}
+  </div>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 0 20px;
+  }
+
+  h1 {
+    text-align: center;
+    margin-top: 40px;
+    margin-bottom: 20px;
+  }
+
+  .job-listings {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 20px;
+    margin-bottom: 40px;
+  }
+</style>
