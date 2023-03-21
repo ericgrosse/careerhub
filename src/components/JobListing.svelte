be12e1e5f30f2c1438f8542a9c46c6dbbcba5ee0
Eric Grossé <ericgrosse1@gmail.com>
2023-03-21 14:10:53 -0400

    Added JobListing component

---
+++ src/components/JobListing.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let jobTitle;
+  export let jobDescription;
+  export let jobLocation;
+  export let jobType;
+  export let jobSalary;
+</script>
+
+<div class="job-listing">
+  <h2>{jobTitle}</h2>
+  <p>{jobDescription}</p>
+  <ul>
+    <li><strong>Location:</strong> {jobLocation}</li>
+    <li><strong>Type:</strong> {jobType}</li>
+    <li><strong>Salary:</strong> {jobSalary}</li>
+  </ul>
+</div>
